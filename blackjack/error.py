deffbd9eb60ef2e8d609d8dace3f484aa43e7691
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-09-04 19:01:56 +0800

    Define base error class for blackjack module

---
+++ blackjack/error.py
@@ -0,0 +1,10 @@
+# card.py
+#
+# Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+# Licensed under MIT
+# Version 1.0.0-alpha
+
+class Error(Exception):
+  """Base class for exceptions in this module."""
+  pass
+
