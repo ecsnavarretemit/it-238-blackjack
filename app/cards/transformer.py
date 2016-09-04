d3edf03dd172c30f39d749a6fd230ce2eaa429fa
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-09-04 22:41:18 +0800

    Add deserialization utilities for card and plain text

---
+++ app/cards/transformer.py
@@ -0,0 +1,62 @@
+# transformer.py
+#
+# Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+# Licensed under MIT
+# Version 1.0.0-alpha
+
+from app.cards.card import Card
+from app.cards.error import TransformerError
+import re
+
+class Transformer(object):
+  """Base class for card transformers in this module."""
+  pass
+
+class CardToTextTransformer(Transformer):
+  """Transformer class for cards to text for sending over the network"""
+
+  def __init__(self, card=None):
+    Transformer.__init__(self)
+
+    # Initialize card to None
+    self.card = None
+
+    if card != None:
+      self.set_card(card)
+
+  def set_card(self, card:Card):
+    self.card = card
+
+  def transform(self):
+    if self.card == None:
+      raise TransformerError("Set a card first before transforming it")
+
+    return "%s of %s" % (self.card.get_face_value(), self.card.get_shape())
+
+class TextToCardTransformer(Transformer):
+  """Transformer class for deserializing text back to card instances"""
+
+  def __init__(self, text=None):
+    Transformer.__init__(self)
+
+    # Initialize card to None
+    self.text = None
+
+    if text != None:
+      self.set_text(text)
+
+  def set_text(self, text):
+    self.text = text
+
+  def transform(self):
+    if self.text == None or self.text == '':
+      raise TransformerError("Set a serialized text of the card first before transforming it")
+
+    matches = re.search('([0-9JQKA]+) of (diamond|heart|spade|clover)', self.text)
+
+    if not matches:
+      raise TransformerError("Cant deserialize card: %s" % self.text)
+
+    return Card(matches.group(2), matches.group(1))
+
+
