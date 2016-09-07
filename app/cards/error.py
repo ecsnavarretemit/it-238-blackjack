1fd5eba8ae8f07af755d2a312a7bf13bfac73888
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-09-07 21:12:31 +0800

    Bump version to 1.0.0

--- app/cards/error.py
+++ app/cards/error.py
@@ -2,7 +2,7 @@
 #
 # Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
 # Licensed under MIT
-# Version 1.0.0-alpha
+# Version 1.0.0
 
 from app.error import Error
 
--- app/error.py
+++ app/error.py
@@ -2,7 +2,7 @@
 #
 # Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
 # Licensed under MIT
-# Version 1.0.0-alpha
+# Version 1.0.0
 
 class Error(Exception):
   """Base class for exceptions in this module."""
