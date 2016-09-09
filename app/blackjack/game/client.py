ceefbf2a646fa186be87fc9e73b4e78534dab783
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-09-09 13:12:31 +0800

    Bump version to 1.0.2

--- app/blackjack/game/client.py
+++ app/blackjack/game/client.py
@@ -2,5 +2,5 @@
 #
 # Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
 # Licensed under MIT
-# Version 1.0.1
+# Version 1.0.2
 
--- app/cards/transformer.py
+++ app/cards/transformer.py
@@ -2,7 +2,7 @@
 #
 # Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
 # Licensed under MIT
-# Version 1.0.1
+# Version 1.0.2
 
 import re
 from app.cards.card import Card, SHAPES, FACE_VALUES
